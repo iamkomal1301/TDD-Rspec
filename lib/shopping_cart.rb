215fd96ab3857da681697292b1937ed2fe017e36
Komal Arora <komal.arora@quincus.com>
2025-04-29 20:07:44 +0530

    feat: re-modify the cart data

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -4,4 +4,31 @@ class ShoppingCart
   def initialize
     @items = {}
   end
+
+  def add(item, quantity = 1)
+    @items[item] ||= 0
+    @items[item] += quantity
+  end
+
+  def total_items
+    @items.keys.count
+  end
+
+  def total_quantity
+    @items.values.sum
+  end
+
+  def remove(item)
+    @items.delete(item)
+  end
+
+  def clear
+    @items.clear
+  end
+
+  def total_price(prices = {}, discount: 0)
+    total = @items.sum { |item, qty| (prices[item] || 0) * qty }
+    total -= total * discount
+    total.round(2)
+  end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -5,4 +5,47 @@ RSpec.describe ShoppingCart do
     cart = ShoppingCart.new
     expect(cart.items).to eq({})
   end
+
+  it 'adds a single item' do
+    cart = ShoppingCart.new
+    cart.add('apple', 2)
+    expect(cart.items).to eq({ 'apple' => 2 })
+  end
+
+  it 'returns total number of unique items' do
+    cart.add('apple', 2)
+    cart.add('banana', 1)
+    expect(cart.total_items).to eq(2)
+  end
+
+  it 'returns total quantity of all items' do
+    cart.add('apple', 2)
+    cart.add('banana', 3)
+    expect(cart.total_quantity).to eq(5)
+  end
+
+  it 'removes an item completely' do
+    cart.add('apple', 2)
+    cart.remove('apple')
+    expect(cart.items).not_to include('apple')
+  end
+
+  it 'clears all items' do
+    cart.add('apple', 2)
+    cart.clear
+    expect(cart.items).to be_empty
+  end
+
+  it 'calculates total price' do
+    cart.add('apple', 2)
+    cart.add('banana', 1)
+    prices = { 'apple' => 3.0, 'banana' => 2.0 }
+    expect(cart.total_price(prices)).to eq(8.0)
+  end
+
+  it 'applies a 10% discount' do
+    cart.add('apple', 2)
+    prices = { 'apple' => 5.0 }
+    expect(cart.total_price(prices, discount: 0.10)).to eq(9.0)
+  end
 end
